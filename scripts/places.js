744f96b8b203a9c96459ab104f25a15914e32254
Sergey Kouzma <sergey.kouzma@gmail.com>
2022-12-10 22:02:10 +0300

    Добавление новой карточки

--- scripts/places.js
+++ scripts/places.js
@@ -1,18 +1,26 @@
 const addPlaceButton = document.querySelector(".profile__plus");
+const cardsContainer = document.querySelector(".elements");
 
 addPlaceButton.addEventListener("click", function () {
     const addPlaceFormTemplate = document.querySelector("#add-place-template").content;
     const addPlaceFormElement = addPlaceFormTemplate.querySelector(".add-place").cloneNode(true);
     
     addEventsToPlaceForm(addPlaceFormElement);
-    openPopup(addPlaceFormElement);
-    
+    openPopup(addPlaceFormElement);    
 });
+
 function addEventsToPlaceForm(addPlaceFormElement) {
-    const addPlaceform = addPlaceFormElement.querySelector("form");
-    console.log(addPlaceform);
+    const fieldName = addPlaceFormElement.querySelector(".form__field");
+    const cardTemplate = document.querySelector("#card-template").content;
+    const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+    const blockName = cardElement.querySelector(".card__header")
+    
+    const addPlaceform = addPlaceFormElement.querySelector(".form");
     addPlaceform.addEventListener("submit", function (event){
         event.preventDefault();
+        blockName.textContent = fieldName.value;
+        cardsContainer.append(cardElement);
         closePopup();
+        
     });
 }
