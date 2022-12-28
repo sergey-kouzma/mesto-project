b8723eae3955c61c6ac947aa54e3475ddeb09ffa
Sergey Kouzma <sergey.kouzma@gmail.com>
2022-12-28 16:39:34 +0300

    Исправления согласно код-ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,17 @@
-let closeButtons = document.querySelectorAll('.popup__close');
+const closeButtons = document.querySelectorAll('.popup__close');
 
 for (let closeButton of closeButtons) {
     addCloseButtonEvent(closeButton);
 }
 
 function addCloseButtonEvent(closeButton) {
+    const popupToClose = closeButton.closest('.popup');
     closeButton.addEventListener("click", function () {
-        closePopup(closeButton.closest('.popup'));
+        closePopup(popupToClose);
     });
 }
 
-
-
-function openPopup(popup, withoutBorders = false) {
+function openPopup(popup) {
     popup.classList.add("popup_opened");
 }
 
@@ -20,18 +19,15 @@ function closePopup (popup) {
     popup.classList.remove("popup_opened");
 }
 
-
-
+// Часть, которая относится к карточке
+// Оставил здесь, а не вверху файла, чтобы легче было потом выносить в отдельный файл, когда такая задача будет
 const cardsContainer = document.querySelector(".elements");
-
-function createCardFormElement() {
-    const addPlaceFormTemplate = document.querySelector("#add-place-template").content;
-    const addPlaceFormElement = addPlaceFormTemplate.querySelector(".add-place").cloneNode(true);
-    return addPlaceFormElement;
-}
+const cardTemplate = document.querySelector("#card-template").content;
+const popupWithBigImage = document.querySelector('.popup_big-image');
+const bigImg = popupWithBigImage.querySelector(".full-picture__img");
+const bigImgText = popupWithBigImage.querySelector(".full-picture__text");
 
 function createCardElement() {
-    const cardTemplate = document.querySelector("#card-template").content;
     const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
     return cardElement;
 }
@@ -66,63 +62,77 @@ const initialCards = [
   ///////////////////////////////////////
 
 initialCards.forEach(function (element) {
-    addCard(element.name, element.link);
+    addCard({
+        title: element.name, 
+        link: element.link
+    });
 });
-function addCard(title, link) {
-    const cardElement = createCardElement();
+function addCard(cardInitData) {
+    const cardElement = createCard(cardInitData);
+    renderCard(cardElement);
+}
+
+function createCard(cardInitData) {
+    const title = cardInitData.title;
+    const link = cardInitData.link;
+    const cardElement = createCardElement(); // такой стиль читаемее и рекомендуется, например, Бобом Мартином в Книге Чистый год
+    // const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+    // незакомментированная строка выглядит гораздо читемее, чем закомментированная
     const cardImg = cardElement.querySelector(".card__img");
     const blockName = cardElement.querySelector(".card__header");
     const cardLike = cardElement.querySelector(".card__like");
     const cardDelete = cardElement.querySelector(".card__delete");
-    const bigImg = cardElement.querySelector(".full-picture__img");
-    const bigImgText = cardElement.querySelector(".full-picture__text");
-
     cardImg.setAttribute("src", link);
     cardImg.setAttribute("alt", title);
-    bigImg.setAttribute("src", link);
-    bigImg.setAttribute("alt", title);
     blockName.textContent = title;
-    bigImgText.textContent = title;
-    cardDelete.addEventListener("click", function() {
-        cardDelete.parentElement.remove();
-    });
     
+    cardDelete.addEventListener("click", function() {
+        cardDelete.closest('.card').remove();
+    });
     cardImg.addEventListener("click", function() {
-        openPopup(cardElement.querySelector('.popup'), true);
-        addCloseButtonEvent(cardElement.querySelector('.popup__close'));
+        bigImg.setAttribute("src", link);
+        bigImg.setAttribute("alt", title);
+        bigImgText.textContent = title;
+        openPopup(popupWithBigImage);
     });
     cardLike.addEventListener("click", function() {
         cardLike.classList.toggle("card__like_active");
     });
-    addCloseButtonEvent(cardElement.querySelector('.popup__close'));
-    cardsContainer.prepend(cardElement);  
-    
 
+    return cardElement;
 }
 
+function renderCard(cardElement) {
+    cardsContainer.prepend(cardElement);
+}
 
-const addPlaceButton = document.querySelector(".profile__plus");
+// Часть относящаяся к добавлению мест
+const popupAddPlace = document.querySelector('.popup_place-add');
+const buttonAddPlace = document.querySelector(".profile__plus");
 
-const placeFieldImg = document.querySelector(".add-place__img")
-const placeFieldName = document.querySelector(".add-place__name");
-const addPlaceform = document.querySelector(".add-place__form");
+const placeFieldImg = popupAddPlace.querySelector(".add-place__img")
+const placeFieldName = popupAddPlace.querySelector(".add-place__name");
+const formAddPlace = popupAddPlace.querySelector(".add-place__form");
 addEventsToPlaceForm();
 
-addPlaceButton.addEventListener("click", function () {
-    openPopup(addPlaceform.closest('.popup'));    
+buttonAddPlace.addEventListener("click", function () {
+    openPopup(popupAddPlace);    
 });
 
 function addEventsToPlaceForm() {
-    addPlaceform.addEventListener("submit", function (event){
+    formAddPlace.addEventListener("submit", function (event){
         event.preventDefault();
-        addCard(placeFieldName.value, placeFieldImg.value);
-        placeFieldName.value = "";
-        placeFieldImg.value = "";
-        closePopup(addPlaceform.closest('.popup'));
+        addCard({
+            title: placeFieldName.value, 
+            link: placeFieldImg.value
+        });
+        formAddPlace.reset();
+        closePopup(popupAddPlace);
     });
 }
 
-
+// Часть про редактирование профайла
+const popupProfileEdit = document.querySelector('.popup_profile-edit');
 const editProfileButton = document.querySelector(".profile__edit-button");
 
 const profileName = document.querySelector(".profile__name");
@@ -135,8 +145,8 @@ const fieldDescription= document.querySelector(".edit-profile__description");
 addEventsToProfileForm();
 
 editProfileButton.addEventListener("click", function () {
-    setFields(editProfileForm);
-    openPopup(editProfileForm.closest('.popup'));
+    setFields(); // вынос кода в отдельный метод делает его более читаемым. Согласно рекомендациям Боба Мартина Чистый Код
+    openPopup(popupProfileEdit);
 });
 
 function setFields() {
@@ -149,9 +159,6 @@ function addEventsToProfileForm() {
         event.preventDefault();
         profileName.textContent = fieldName.value;
         profileDescription.textContent = fieldDescription.value;
-        closePopup (editProfileForm.closest('.popup'));
+        closePopup (popupProfileEdit);
     });
 }
-
-
-    
