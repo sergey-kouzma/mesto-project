c9837df088ec66f2bce573655277f2d54d648362
yndx-handbook <henchfiend@yandex.ru>
2023-04-09 07:48:13 +0300

    рефакторинг класса Card ч1
    - создание Template выведено в отдельный метод
    - уменьшино количество аргументов которые принимает класс Card, что позитивно сказывается на его вызове

--- src/components/card.js
+++ src/components/card.js
@@ -1,32 +1,33 @@
 import { openPopup } from "./modal.js"
-// import { removeCardFromServer, sendLikeToCardToServer, sendDisLikeToCardToServer } from "./api"
 import { Api } from "./Api.js";
 import { apiConfig } from "./consts/api-consts.js";
 import Section from './Section';
 
-// const profile = document.querySelector(".profile");
 const cardsContainer = '.elements';
-// const cardTemplate = document.querySelector("#card-template").content;
-const cardTemplate = document.querySelector("#card-template").content.querySelector(".card");
 const popupWithBigImage = document.querySelector('.popup_big-image');
 const bigImg = popupWithBigImage.querySelector(".full-picture__img");
 const bigImgText = popupWithBigImage.querySelector(".full-picture__text");
 
 export default class Card {
-  constructor({link, title, id, likes, isOwnCard, ownerId, hasOwnLike}) {
-    this._link = link;
-    this._title = title;
-    this._id = id;
-    this._likes = likes;
+  constructor(data, {isOwnCard, hasOwnLike}) {
+    this._link = data.link;
+    this._title = data.name;
+    this._id = data._id;
+    this._likes = data.likes.length;
+    this._ownerId = data.ownerId;
     this._isOwnCard = isOwnCard;
-    this._ownerId = ownerId;
     this._hasOwnLike = hasOwnLike;
   }
 
-  // addCardToSite() {
-  //   this.createCard();
-  //   this._renderCard();
-  // }
+  _getElement() {
+    const cardElement = document
+      .querySelector("#card-template")
+      .content
+      .querySelector(".card")
+      .cloneNode(true);
+
+    return cardElement;
+  }
 
   _openBigImage() {
     bigImg.setAttribute("src", this._link);
@@ -46,8 +47,6 @@ export default class Card {
   }
 
   _addLikesListener() {
-    // console.log(this._cardLike);
-    // console.log(this);
     this._cardLike.addEventListener("click", () => {
       
       if (this._cardLike.classList.contains("card__like_active")) {
@@ -70,19 +69,19 @@ export default class Card {
   }
 
   createCard() {
-    const cardElement = cardTemplate.cloneNode(true);
-    this._cardImg = cardElement.querySelector(".card__img");
-    this._cardLikesAmount = cardElement.querySelector(".card__likes-amount");
-    this._blockName = cardElement.querySelector(".card__header");
-    this._cardLike = cardElement.querySelector(".card__like");
-    this._cardDelete = cardElement.querySelector(".card__delete");
+    this._cardElement = this._getElement();
+    this._cardImg = this._cardElement.querySelector(".card__img");
+    this._cardLikesAmount = this._cardElement.querySelector(".card__likes-amount");
+    this._blockName = this._cardElement.querySelector(".card__header");
+    this._cardLike = this._cardElement.querySelector(".card__like");
+    this._cardDelete = this._cardElement.querySelector(".card__delete");
     if (!this._isOwnCard) {
         this._cardDelete.remove();
     }
     
-    this._cardImg.setAttribute("src", this._link);
-    this._cardImg.setAttribute("alt", this._title);
-    this._blockName.textContent = this._title
+    this._cardImg.src = this._link;
+    this._cardImg.alt = this._title;
+    this._blockName.textContent = this._title;
     this._cardLikesAmount.textContent = this._likes;
     if (this._hasOwnLike) {
       this._cardLike.classList.add("card__like_active");
@@ -91,27 +90,17 @@ export default class Card {
     this._addLikesListener();
     this._addRemoveListener();
 
-
     this._cardImg.addEventListener("click", () => this._openBigImage());
 
-    return cardElement;
+    return this._cardElement;
   }
- 
-  // _renderCard() {
-  //   cardsContainer.prepend(this._cardElement);
-  // }
 } 
- 
  
 function setInitialCards(data, userId) {
   const cardList = new Section({
     renderer: (item) => {
-      const card = new Card({
-        title: item.name,
-        link: item.link,
-        likes: item.likes.length,
-        id: item._id,
-        ownerId: item.owner._id,
+      const card = new Card(
+        item,{
         hasOwnLike: item.likes.find(item => item._id === userId) != undefined,
         isOwnCard: userId === item.owner._id
       });
